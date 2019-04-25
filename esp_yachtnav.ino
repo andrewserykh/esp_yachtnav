9bfbc52c5cff45e247c4f218bc63752d8155f8bf
Andrew serykh <andrew.serykh@gmail.com>
2019-04-25 14:30:06 +0700

    Simple link protocol
    Простой протокол связи вместо modbus
    управление выходными реле на nano

--- esp_yachtnav.ino
+++ esp_yachtnav.ino
@@ -1,7 +1,8 @@
-/* v2.05
+/* v2.10
 GPS и компас вынесены на nano
 WS обработка связи websocket
 NMEA класс создания сообщений
+Обмен с nano по короткому протоколу
 
 В процессе создания Якорный аларм
 ---
@@ -18,24 +19,23 @@ https://github.com/Links2004/arduinoWebSockets
 #include <ArduinoOTA.h>       // OTA update
 #include "ESP32_Servo.h"      // Servo PWM motor
 #include "html.h"             // Содержимое web интерфейса
-#include "modbusrtu.h"        // Протокол ModbusRTU
 #include "gpsnav.h"           // Класс GPS навигации
 
 #define AccelerateMotorPin  27  //ШИМ мотора управления акселератором (ESP32: 0(used by on-board button),2,4,5(used by on-board LED),12-19,21-23,25-27,32-33)
   
-#define GEAR_N 0      //Передача N
-#define GEAR_F 1      //Передача F
-#define GEAR_R 2      //Передача R
+#define GEAR_N          0      //Передача N
+#define GEAR_F          1      //Передача F
+#define GEAR_R          2      //Передача R
 
-#define MODE_WIFI     0
-#define MODE_MOTOR    1
-#define MODE_SAIL     2
-#define MODE_ANCHOR   3
-#define MODE_NAV      4
-#define MODE_SETUP    9
-#define MODE_OTA      99
+#define MODE_WIFI       0
+#define MODE_MOTOR      1
+#define MODE_SAIL       2
+#define MODE_ANCHOR     3
+#define MODE_NAV        4
+#define MODE_SETUP      9
+#define MODE_OTA        99
 
-#define T_MS_NANO     500   //ms интервал опроса периферии
+#define T_MS_NANO       1000   //ms интервал отправки команд
 
 Preferences prefs;  //EEPROM объект
 
@@ -65,14 +65,39 @@ int ANCHOR_DRIFT;         //Смещение от точки якорения
 int ANCHOR_DRIFT_MAX;     //Максимальное смещение
 bool LINKERROR;           //Нет связи с Arduino-Nano
 
-modbusrtu devModbus;
 char SerialNanoIn[64];    //буфер приема 
 byte SerialNanoInLen;     //заполнение буфера
 long SerialNanoInMillis;
-gpsnav  GPS;
-gpspoi  PoiAnchor;
+gpsnav GPS;
+gpspoi PoiAnchor;
 long ms_update, ms_nano, ms_wifi, ms_btn0;
 bool isAJAX = false;      //Этот ответ сервера - AJAX-ответ
+
+
+struct { //Структура пакета данных измерений передаваемых из nano в esp
+  byte Header; //заголовок пакета 0x65
+  byte Length; //длина пакета
+  uint16_t CompassCourse;
+  uint16_t GpsCourse;
+  uint16_t GpsSpeedKmh;
+  float GpsSpeedKnots;
+  float GpsLat;
+  float GpsLng;
+  uint16_t GpsAlt;
+  uint16_t GpsHour;
+  uint16_t GpsMinute;
+  uint16_t GpsSecond;
+  uint16_t GpsDay;
+  uint16_t GpsMonth;
+  uint16_t GpsYear;
+} datanano;
+
+struct { //Структура пакета команд передаваемых из esp в nano
+  byte Header;
+  byte Length;
+  byte Relay;
+} dataesp;
+
 //временные переменные - удалить:
 int tempCnt=0;
 
@@ -92,10 +117,10 @@ void setup() {
   ANCHOR_DRIFT_MAX = prefs.getUInt("drift", 20);
   prefs.end();
 
-  pinMode(0, INPUT);        //build-in btn
-  pinMode(25, OUTPUT);      //bulid-in led
-  pinMode(16, OUTPUT);      //RST
-
+  pinMode(0,INPUT);       //build-in btn
+  pinMode(25,OUTPUT);     //bulid-in led
+  pinMode(16,OUTPUT);     //RST
+  
   servoAccelerate.attach(AccelerateMotorPin, 1000, 2000); //for MG995 large servo, use 1000us and 2000us
     
   digitalWrite(16, LOW); delay(50); digitalWrite(16, HIGH);    // set GPIO16 low to reset OLED HIGH to running OLED
@@ -141,6 +166,7 @@ void setup() {
   delay(1000);
 
   //MODE = MODE_WIFI;
+  dataesp.Relay=0x00;
   ms_update = ms_wifi = ms_nano = millis();
 }
 
@@ -155,24 +181,28 @@ void loop() {
     ESP.restart();
   }
 
-//функции обработки управления  
+//---Блок Управления
 
-  servoAccelerate.write(map(M_THROTTLE, 0, 100, 0, 180));
+  servoAccelerate.write(map(M_THROTTLE, 0, 100, 0, 180)); //Управлением приводом акселератора
+
+  if (millis() - ms_nano > T_MS_NANO) { //отправка команд на Arduino-Nano
+    dataesp.Header = 0x69;  //Заголовок пакета "E"
+    dataesp.Length = (byte)sizeof(dataesp);
+    bitWrite ( dataesp.Relay,0,HIGH );
+    bitWrite ( dataesp.Relay,1,LOW );
+    bitWrite ( dataesp.Relay,2,HIGH );
+    bitWrite ( dataesp.Relay,3,LOW );
+    for (int i=0; i<sizeof(dataesp); i++) SerialNano.write( ((char*)&dataesp)[i] );
+    ms_nano = millis();
+  }
+
+//---Конец Блока Управления
 
   if (millis() - ms_update > 3000) {
     if (MODE == MODE_WIFI) display_WIFI();
     if (MODE == MODE_SAIL||MODE == MODE_MOTOR) display_NAV();
+    LINKERROR=true;
     ms_update = millis();
-  }
-
-  if (millis() - ms_nano > T_MS_NANO) { //отправка modbus запроса Arduino-Nano
-    if (!devModbus.online) devModbus.error++;
-    LINKERROR=false;
-    if (devModbus.error>10) LINKERROR=true;
-    devModbus.make(01, 4, 0, 16);
-    devModbus.online=false;
-    SerialNano.write(devModbus.packetout, devModbus.packetout_len);
-    ms_nano = millis();
   }
 
   if (digitalRead(0) == HIGH) ms_btn0 = millis();
@@ -222,38 +252,41 @@ void loop() {
 
   while (SerialNano.available() > 0) {
     char SerialChar = (char)SerialNano.read();
-    devModbus.packet[devModbus.packet_length] = SerialChar;
-    devModbus.packet_length++;
+    SerialNanoIn[SerialNanoInLen] = SerialChar;
+    SerialNanoInLen++;
 
     SerialNanoInMillis = millis();
   }
   
-  if (devModbus.packet_length > 0 && (millis() - SerialNanoInMillis > 100)) {
-    if (devModbus.ispacket()) {
-      devModbus.online=true;
-      devModbus.error=0;
-      HDG = devModbus.getint(0);
-      GPS.hdg = devModbus.getint(2);      
-      SOGkmh = devModbus.getint(4);
-      SOG = devModbus.getfloat(6);
-      GPS.lat = devModbus.getfloat(10);
-      GPS.lng = devModbus.getfloat(14);
-      GPS.alt = devModbus.getint(18);
-      GPS.hour = devModbus.getint(20);
-      GPS.minute = devModbus.getint(22);
-      GPS.second = devModbus.getint(24);
-      GPS.day = devModbus.getint(26);
-      GPS.month = devModbus.getint(28);
-      GPS.year = devModbus.getint(30);
+  if (SerialNanoInLen > 0 && (millis() - SerialNanoInMillis > 100)) {
+    
+    //for (int q = 0; q < SerialNanoInLen; q++) { //вывод принятого пакета на консоль
+    //Serial.print((byte)SerialNanoIn[q], HEX); Serial.print(" ");
+    //} Serial.println(" ");
+    
+    if (SerialNanoIn[0] == 0x65){ //Проверка заголовка пакета 0x65
+      if (sizeof(datanano)>=SerialNanoInLen) for (int i=0; i<SerialNanoInLen; i++) ((byte*)&datanano)[i] = SerialNanoIn[i]; //загрузка структуры данных
+      LINKERROR = false;
+      HDG = datanano.CompassCourse;
+      GPS.hdg = datanano.GpsCourse;      
+      SOGkmh = datanano.GpsSpeedKmh;
+      SOG = datanano.GpsSpeedKnots;
+      GPS.lat = datanano.GpsLat;
+      GPS.lng = datanano.GpsLng;
+      GPS.alt = datanano.GpsAlt;
+      GPS.hour = datanano.GpsHour;
+      GPS.minute = datanano.GpsMinute;
+      GPS.second = datanano.GpsSecond;
+      GPS.day = datanano.GpsDay;
+      GPS.month = datanano.GpsMonth;
+      GPS.year = datanano.GpsYear;
 
       if (ANCHOR){ //если якорь опущен, расчет смещения от точки якорения
         ANCHOR_DRIFT = GPS.distance(GPS.lat,GPS.lng,PoiAnchor.lat,PoiAnchor.lng);
       } //anchor
-       //for (int q = 0; q < devModbus.packet_length; q++) { //вывод принятого пакета на консоль
-       //Serial.print((byte)devModbus.packet[q], HEX); Serial.print(" ");
-       //} Serial.println(" ");
-    }
-    devModbus.packet_length=0;
+    } //0x65
+    
+    SerialNanoInLen=0;
   } 
   
 
